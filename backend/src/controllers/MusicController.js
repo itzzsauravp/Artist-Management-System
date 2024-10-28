83bd8c321a9f0ccdf0b0cfe70d6fb51783b2849a
Saurav Parajulee <social.saurav2003@gmail.com>
2024-10-28 21:42:52 +0545

    (partly incomplete but functional)

---
+++ backend/src/controllers/MusicController.js
@@ -0,0 +1,9 @@
+class MusicController {
+  constructor(connection) {
+    this.connection = connection;
+  }
+
+  // routes logic for music goes here..
+}
+
+export default MusicController;
---
+++ backend/src/routes/musicRoute.js
@@ -0,0 +1,16 @@
+import { Router } from "express";
+import MusicController from "../controllers/MusicController.js";
+const musicRouter = Router();
+(async () => {
+    try {
+      const connection = await connectionFunction();
+      const musicControllerInstance = new MusicController(connection);
+    
+      // routes for music goes here...
+
+    } catch (error) {
+      console.error("Failed to initialize UserController:", error);
+    }
+  })();
+
+export default musicRouter;
